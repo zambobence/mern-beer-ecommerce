e1139d510684275b8344803d5f657dc70f6ea18f
Bence Zambo <zambobence23@gmail.com>
2023-09-01 11:52:04 -0500

    updated logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,12 @@
 import React from 'react'
-import { Text } from '@chakra-ui/react'
+import { Text, Box, Icon, Flex } from '@chakra-ui/react'
+import {IoBeerOutline} from "react-icons/io5";
+
 export default function Logo() {
   return (
-    <Text fontWeight={700}>Bee(R)Commerce</Text>
+    <Flex color={"brand.300"} alignItems={"center"} gap={".2rem"}>
+      <Icon as={IoBeerOutline}/>
+      <Text fontWeight={700}>Bee(R)Commerce</Text>
+    </Flex>
   )
 }
